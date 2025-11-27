6cc41a40a225f4546e99135f0cd422e0f020f69c
juan9321 <juanfariasbr@gmail.com>
2025-11-27 11:00:00 -0300

    feat: aplicação Spring Boot e configuração padrão do módulo 3

---
+++ src/main/java/br/com/frettec/modulo3/lambda_kafka/LambdaKafkaApplication.java
@@ -0,0 +1,11 @@
+package  br.com.frettec.modulo3.lambda_kafka; 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdaKafkaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LambdaKafkaApplication.class, args);
+    }
+}
---
+++ src/test/java/br/com/frettec/modulo3/lambda_kafka/LambdaKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.frettec.modulo3.lambda_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambdaKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
